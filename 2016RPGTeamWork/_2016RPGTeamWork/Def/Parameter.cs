607b26b806a5faadc10dabc6eaebea6273e62f2d
ziyansh2 <ziyansh2_007@yahoo.co.jp>
2016-12-15 15:19:18 +0900

    2016.12.15 柏 Parameter.cs追加

---
+++ 2016RPGTeamWork/_2016RPGTeamWork/Def/Parameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2016RPGTeamWork.Def
+{
+    static class Parameter
+    {
+        public const int MaxActionPercent = 100;
+
+    }
+}

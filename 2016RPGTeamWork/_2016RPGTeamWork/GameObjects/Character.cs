caddeac046dbd8adeb6f182b0e5c12993d5a968b
ziyansh2 <ziyansh2_007@yahoo.co.jp>
2016-12-15 15:24:13 +0900

    2016.12.15 柏 クラス追加： Character.cs CharacterInfo.cs

---
+++ 2016RPGTeamWork/_2016RPGTeamWork/GameObjects/Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2016RPGTeamWork.GameObjects
+{
+    public abstract class Character
+    {
+        private CharacterInfo characterInfo;
+
+        int hp; //残っているヒットポイント
+        int mp; //残っている魔法ポイント
+
+        int offenceModifier; //ゲーム中の攻撃力へのバフ・デバフの等
+        int defenceModifier; //…防御力
+        int moModifier; //…魔法攻撃力
+        int mdModifier; //…魔法防御宇力
+        int speedModifier; //…素早さ
+
+        public Character(CharacterInfo ci)
+        {
+            characterInfo = ci;
+            hp = characterInfo.MaxHP;
+            mp = characterInfo.MaxMP;
+        }
+
+        public void Damage(int amount)
+        {
+            hp -= amount;
+            HpClip();
+        }
+
+        private void HpClip()
+        {
+            if (hp > MaxHP) hp = MaxHP;
+            else if (hp < 0) hp = 0;
+        }
+
+        public int MaxHP { get { return characterInfo.MaxHP; } }
+        public int MaxMP { get { return characterInfo.MaxMP; } }
+        public int Offence { get { return characterInfo.Offence; } }
+        public int Defence { get { return characterInfo.Defence; } }
+        public int MagicOffence { get { return characterInfo.MagicOffence; } }
+        public int MagicDefence { get { return characterInfo.MagicDefence; } }
+        public int Speed { get { return characterInfo.Speed; } }
+    }
+}
---
+++ 2016RPGTeamWork/_2016RPGTeamWork/GameObjects/CharacterInfo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2016RPGTeamWork.GameObjects
+{
+    public struct CharacterInfo
+    {
+        string name; //名前
+        int maxHP; //最高ヒットポイント
+        int maxMP; //最高魔法ポイント
+        int offence; //攻撃力
+        int defence; //防御力
+        int magicOffence; //魔法の威力
+        int magicDefence; //魔法に対する防御
+        int speed; //素早さ
+        int level;
+
+        public CharacterInfo(string name, int maxHP, int maxMP, int offence, int defence, int magicOffence, int magicDefence, int speed)
+        {
+            this.name = name;
+            this.maxHP = maxHP;
+            this.maxMP = maxMP;
+            this.offence = offence;
+            this.defence = defence;
+            this.magicOffence = magicOffence;
+            this.magicDefence = magicDefence;
+            this.speed = speed;
+            level = 1;
+        }
+
+        public CharacterInfo(string name, int maxHP, int maxMP, int offence, int defence, int magicOffence, int magicDefence, int speed, int level)
+        {
+            this.name = name;
+            this.maxHP = maxHP;
+            this.maxMP = maxMP;
+            this.offence = offence;
+            this.defence = defence;
+            this.magicOffence = magicOffence;
+            this.magicDefence = magicDefence;
+            this.speed = speed;
+            this.level = level;
+        }
+
+        public int MaxHP
+        {
+            get { return maxHP; }
+            set { maxHP = value; }
+        }
+        public int MaxMP
+        {
+            get { return maxMP; }
+            set { maxMP = value; }
+        }
+        public int Offence
+        {
+            get { return offence; }
+            set { offence = value; }
+        }
+        public int Defence
+        {
+            get { return defence; }
+            set { defence = value; }
+        }
+        public int MagicOffence
+        {
+            get { return magicOffence; }
+            set { magicOffence = value; }
+        }
+        public int MagicDefence
+        {
+            get { return magicDefence; }
+            set { magicDefence = value; }
+        }
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+    }
+}
